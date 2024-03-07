980e9f69681ad0d2f274c1573623ce27cdeaa0f2
Robert <dinizrobert2002@gmail.com>
2024-03-07 00:40:52 -0300

    update django

--- backend/despesas/serializers.py
+++ backend/despesas/serializers.py
@@ -9,13 +9,18 @@ class CategoriaSerializers(serializers.ModelSerializer):
 class DespesaSerializers(serializers.ModelSerializer):
 
     categoria = serializers.PrimaryKeyRelatedField(queryset=Categorias.objects.all())
+    categoria_imagem = serializers.SerializerMethodField()
 
     class Meta:
         model = Despesas
-        fields = ('id', 'categoria', 'valor', 'data')
+        fields = ('id', 'categoria', 'valor', 'data', 'categoria_imagem')
+
+    def get_categoria_imagem(self, instance):
+        return instance.categoria.imagem.url if instance.categoria.imagem else None
 
     # Representar nome da categoria na API
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         representation['categoria'] = instance.categoria.nome
+        
         return representation
